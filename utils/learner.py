beb245524b8351ce6d1e621ae4386d836ea34a04
Thomas Reeves <tjtommy5000@gmail.com>
2021-05-14 10:06:54 -0700

    Restructured Utils files. Custom dataseet PlantPathologyDataset added. Small Example CNN works.

---
+++ utils/learner.py
@@ -0,0 +1,67 @@
+import torch
+import torch.nn as nn
+from torch.autograd import Variable
+import numpy as np
+from torch.utils.data import sampler
+
+class Flatten(nn.Module):
+    '''from Hw3'''
+    def forward(self, x):
+        N, C, H, W = x.size() # read in N, C, H, W
+        return x.view(N, -1)  # "flatten" the C * H * W values into a single vector per image
+
+
+def train(model, training_data, loss_fn, optimizer, dtype = torch.FloatTensor, num_epochs = 1,print_every = 10):
+    for epoch in range(num_epochs):
+        print('Starting epoch %d / %d' % (epoch + 1, num_epochs))
+        model.train()
+        for t, (x, y) in enumerate(training_data):
+            print(x.shape,y)
+            x_var = Variable(x.type(dtype))
+            y_var = Variable(y.type(dtype).long())
+
+            scores = model(x_var)
+
+            loss = loss_fn(scores, y_var)
+            if (t + 1) % print_every == 0:
+                print('t = %d, loss = %.4f' % (t + 1, loss.item()))
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+def check_accuracy(model, loader,dtype = torch.FloatTensor,train = True):
+    if train:
+        print('Checking accuracy on validation set')
+    else:
+        print('Checking accuracy on test set')
+    num_correct = 0
+    num_samples = 0
+    model.eval() # Put the model in test mode (the opposite of model.train(), essentially)
+    for x, y in loader:
+        with torch.no_grad():
+            x_var = Variable(x.type(dtype))
+
+        scores = model(x_var)
+        _, preds = scores.data.cpu().max(1)
+        num_correct += (preds == y).sum()
+        num_samples += preds.size(0)
+    acc = float(num_correct) / num_samples
+    print('Got %d / %d correct (%.2f)' % (num_correct, num_samples, 100 * acc))
+    return acc
+
+class ChunkSampler(sampler.Sampler):
+    """Samples elements sequentially from some offset.
+    Arguments:
+        num_samples: # of desired datapoints
+        start: offset where we should start selecting from
+    """
+    def __init__(self, num_samples, start = 0):
+        self.num_samples = num_samples
+        self.start = start
+
+    def __iter__(self):
+        return iter(range(self.start, self.start + self.num_samples))
+
+    def __len__(self):
+        return self.num_samples
---
+++ utils/utils.py
@@ -0,0 +1,66 @@
+from os import walk
+from os import path
+import os
+import numpy as np
+import csv
+from PIL import Image
+from tqdm import tqdm
+ROOT_DIR = '''C:\\Users\\tjtom\PycharmProjects\PlantPathology2021'''
+
+import torch.nn as nn
+
+def getXSamples(numSamples,verbose=False):
+    all_files = []
+    train_images = os.path.join(ROOT_DIR,"train_images")
+    train_csv = os.path.join(ROOT_DIR,"train.csv")
+    print(train_images)
+    for (dirpath, dirnames, filenames) in walk(train_images):
+        all_files.extend(filenames)
+    selected_files = np.random.choice(all_files,numSamples)
+
+    reader = csv.DictReader(open(train_csv)) # read all labels from train.csv
+    labels = {}
+
+    for row in reader:                          # convert to dictionary key = filename, value = label
+        labels[row['image']] = row['labels']
+    X,Y = filenameToFeatureMatrix(selected_files,labels,verbose=False)  # convert liist of filenames as strings to Feature Matrix
+    return X,Y
+def filenameToFeatureMatrix(filenames,all_labels,verbose = False):
+    '''Description: Takes a list of filenames form train_images, and converts them to Feature Matrix X
+        X has the Shape N x C x H x W, where:
+
+        N is the number of datapoints
+        C is the number of channels
+        H is the height of the intermediate feature map in pixels
+        W is the height of the intermediate feature map in pixels
+
+        Returns:
+            X: Feature Matrix as described above.
+            Y: Corresponding labels to X. In the exact same order.'''
+    Y = []
+    X = []
+    for i in tqdm(range(len(filenames)),disable=verbose):
+        try:
+            file = filenames[i]
+            Y.append(all_labels[file])
+            im = Image.open(os.path.join(ROOT_DIR,"train_images",file))
+            im = im.resize((4000, 2672)) # Most frequent size of sample Image. All images must be the same size for CNN
+            a = np.array(im)
+            im.close()
+
+            # seperate Image into 3 channels by color just like with CIFAR10
+            r = a[:,:,0]
+            g = a[:,:,1]
+            b = a[:,:,2]
+
+            X.append(np.asarray([r,g,b]))
+        except (MemoryError):
+            print("Memory Error Found:")
+            print("/t Filename:",file)
+            print("/t Label:",all_labels[file])
+            print("/t ImageObject:",im)
+
+    X = np.asarray(X)
+    Y = np.asarray(Y)
+    return X,Y
+
